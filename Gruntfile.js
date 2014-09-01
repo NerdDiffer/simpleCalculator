c61e6046fcb9979d8cec84013497d196a3bdd9d4
Rafael Espinoza <rafael@rafaelespinoza.com>
2014-08-31 21:38:16 -0700

    remove old draw.js file, adjust Gruntfile task

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,10 +59,6 @@ module.exports = function(grunt) {
         files: ['!dev/js/gen*.js', 'dev/js/input.js', 'dev/js/util.js'],
         tasks: 'concat:inputUtil'
       }
-      //browserify: {
-      //  files: 'dev/js/script.js', 
-      //  tasks: 'browserify:compile'
-      //}
     }
   });
 
