f8cf59abb6c24a67b38867570aa0831174132e11
Rafael Espinoza <rafael@rafaelespinoza.com>
2014-08-31 22:29:32 -0700

    add event listener & boolean screen size query for orientation

--- dev/js/operate.js
+++ dev/js/operate.js
@@ -12,3 +12,7 @@ function validateInput(e) {
 	var validEvents = [0, 8, 13, 37, 40, 41, 42, 43, 45, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 61, 63, 127];
 	return validEvents.indexOf(e.which) >= 0 ? true: false;
 }
+
+function isLandscape() {
+  return window.innerHeight < window.innerWidth;
+}
